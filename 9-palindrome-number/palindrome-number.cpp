55fa668b1a461cec43c4193e9c38372c401f18e2
Tanish Pandey <tanishpandey86@gmail.com>
2025-07-27 12:16:09 +0530

    Time: 3 ms (45.90%) | Memory: 8.5 MB (67.97%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long int rev=0;
+        int temp, fun=x;
+        if (x<0)
+            return false;
+        while(x!=0){
+            temp=x%10;
+            rev=rev*10+temp;
+            x=x/10;
+        }
+        return fun==rev;
+    }
+};
