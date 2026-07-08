391fe3228b543763dfe0d365ac583b4797c1952e
Tanish Pandey <tanishpandey86@gmail.com>
2026-07-08 23:34:13 +0530

    Time: 3 ms (95.12%), Space: 22.9 MB (79.92%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,38 +1,23 @@
 class Solution {
 public:
-    int findmax(vector<int> &v){
-        int maxi = INT_MIN;
-        int n = v.size();
-        for (int i = 0; i < n; i++){
-            maxi = max(maxi, v[i]);
-        }
-        return maxi;
-    }
-
-    long long calculatehours(vector<int> &v, int hourly){
-        long long totalH = 0;
-        int n = v.size();
-        for (int i = 0; i < n; i++){
-            totalH += ceil((double) v[i] / (double) hourly);
-        }
-        return totalH;
-    }
-
     int minEatingSpeed(vector<int>& piles, int h) {
-        int low = 1;
-        int high = findmax(piles);
-        int ans = high;
+        int lo = 1, hi = *max_element(piles.begin(), piles.end());
 
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-            long long totalhrs = calculatehours(piles, mid);
-            if (totalhrs <= h){
-                ans = mid;
-                high = mid - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+            long long hoursNeeded = 0;
+            for (int p : piles) {
+                hoursNeeded += (p + mid - 1) / mid; // ceil(p / mid)
+            }
+
+            if (hoursNeeded <= h) {
+                hi = mid;       // mid works, try slower
             } else {
-                low = mid + 1;
+                lo = mid + 1;   // too slow, speed up
             }
         }
-        return ans;
+
+        return lo;
     }
 };
