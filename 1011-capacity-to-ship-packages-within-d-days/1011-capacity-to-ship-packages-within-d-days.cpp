764862a9477d74a99f6e425784a68533eecd57b3
Tanish Pandey <tanishpandey86@gmail.com>
2026-07-11 15:58:59 +0530

    Time: 7 ms (91.32%), Space: 35.1 MB (19.54%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
-    long long possible(vector<int> &weights, int capacity){
-        long long days = 1;
-        long long currentLoad = 0;
-        int n = weights.size();
-
-        for (int i = 0; i < n; i++){
-            if (currentLoad + weights[i] > capacity){
-                days++;
-                currentLoad = 0;
-            }
-            currentLoad += weights[i];
-        }
-        return days;
-    }
-
-    int shipWithinDays(vector<int>& weights, int days) {
-        int low = *max_element(weights.begin(), weights.end());
-        int high = accumulate(weights.begin(), weights.end(), 0);
-
-        while (low < high){
-            int mid = low + (high - low) / 2;
-
-            if (possible(weights, mid) <= days){
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return low;
-    }
+    int countDays(vector<int>& ws, int tot_cap, int cur_cap = 0, int res = 1) {
+  for (auto w : ws) {
+    cur_cap += w;
+    if (cur_cap > tot_cap) ++res, cur_cap = w;
+  }
+  return res;
+}
+int shipWithinDays(vector<int>& ws, int D) {
+  auto r = accumulate(begin(ws), end(ws), 0);
+  auto l = max(r / D, *max_element(begin(ws), end(ws)));
+  while (l < r) {
+    auto m = (l + r) / 2;
+    if (countDays(ws, m) <= D) r = m;
+    else l = m + 1;
+  }
+  return l;
+}
 };
