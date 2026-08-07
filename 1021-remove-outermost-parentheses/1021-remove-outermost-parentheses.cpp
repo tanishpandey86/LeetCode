30cd6948026905b53ab08239236a5d7d6476adac
Tanish Pandey <tanishpandey86@gmail.com>
2026-08-07 17:32:05 +0530

    Time: 0 ms (100%), Space: 8.9 MB (81.67%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+    string result;
+    int depth = 0;
+    for (char c : s) {
+        if (c == '(') {
+            if (depth > 0) result += c;   // not the outer '('
+            depth++;
+        } else { // c == ')'
+            depth--;
+            if (depth > 0) result += c;   // not the outer ')'
+        }
+    }
+    return result;
+}
+};
